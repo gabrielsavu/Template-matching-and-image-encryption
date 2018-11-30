d6e0e8b7b15123ba471570004d4ad7dd8e18361f
Savu Liviu <gabriel.savu99@gmail.com>
2018-11-30 22:17:14 +0200

    Am adaugat criptarea de imagini

---
+++ image.c
@@ -0,0 +1,6 @@
+/*
+ * image.c
+ * Created by Savu Liviu Gabriel on 29.11.2018.
+ */
+
+#include "image.h"
---
+++ image.h
@@ -0,0 +1,9 @@
+/*
+ * image.h
+ * Created by Savu Liviu Gabriel on 29.11.2018.
+ */
+
+#ifndef PP_PROIECT_IMAGE_H
+#define PP_PROIECT_IMAGE_H
+
+#endif //PP_PROIECT_IMAGE_H
